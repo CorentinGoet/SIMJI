a9a79e30ee5e0ac7339552156b1de1f6fe883772
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-09-06 09:51:33 +0200

    added the remaining operations

--- python_files/assembleur.py
+++ python_files/assembleur.py
@@ -23,8 +23,13 @@ class Assembleur:
 
     def get_regs(self, n, opp):
         word = self.instrList[n]
-        if 1 <= opp <= 14:
-            opp, regs = word.split(" ")  # we only keep the first word
+        if opp != 0:
+            trash, regs = word.split(" ")
+        else:
+            regs = 0
+
+        if 1 <= opp <= 14:  # operations à 3 arguments
+
             regs = regs.split(",")
             r_a = int(regs[0][1:])
             r_b = regs[1]
@@ -39,10 +44,39 @@ class Assembleur:
                r_b = int(r_b)
             return r_a, r_b, r_out, isANum
 
+        elif opp == 15:     # jmp
+            o, r = regs.split(",")
+            if o[0] == "R":
+                isANum = 0
+                o = int(o[1:])
+            else:
+                isANum = 1
+                o = int(o)
+
+            return o, int(r[1:]), isANum
+
+        elif opp in [16, 17]:     # braz ou branz
+            r, a = regs.split(",")
+            return int(r[1:]), int(a)
+
+        elif opp == 18:     # scall
+            return int(regs)
+
+        else:
+            return 0
+
+
+
+
+
+
     def getInstrNum(self, n):
         opp = self.get_opp(n)
         regs = self.get_regs(n, opp)
-        if 1 <= opp <= 14:
+        if opp == 0:
+            instr = 0
+
+        elif 1 <= opp <= 14:
             r_a, r_b, r_out, isANum = regs
             instr = 0
             instr += opp << 27
@@ -51,7 +85,29 @@ class Assembleur:
             instr += r_b << 5
             instr += r_out
 
+        elif opp == 15:     # jmp
+            o, r, isANum = regs
+            instr = 0
+            instr += opp << 27
+            instr += isANum << 26
+            instr += o << 5
+            instr += r
+
+        elif opp in [16, 17]:
+            r, a = regs
+            instr = 0
+            instr += opp << 27
+            instr += r << 22
+            instr += a
+
+        elif opp == 18:
+            n = regs
+            instr = 0
+            instr += opp << 27
+            instr += n
+
         return instr
+
 
     def writeInstr(self, instr, n):
         f = open("../output_files/test.txt", 'a')
--- python_files/main.py
+++ python_files/main.py
@@ -4,3 +4,4 @@ asm = Assembleur("../assembly_program_files/test.txt")
 for i in range(len(asm.instrList)):
     instr = asm.getInstrNum(i)
     asm.writeInstr(instr, i)
+    print(i)
