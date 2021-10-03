8f79ea05043edb0ac71ecf0004b53f28cd54ac99
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-10-04 01:23:01 +0200

    added setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# author Corentin GOETGHEBEUR (SNS 2023)
+# Ce programme permet d'attribuer les permissions d'exécutions aux fichiers exécutables python et de les renommer
+
+chmod +x python_files/asm.py
+chmod +x python_files/iss.py
+
+mv python_files/asm.py python_files/asm
+mv python_files/iss.py python_files/iss
+
+cd python_files
+echo "Installation terminée, essayez ./asm ou ./iss !"
