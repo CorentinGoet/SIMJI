af8b2dfdb5979452867c98954dd036f5196f64fc
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-10-04 08:37:27 +0200

    correction

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 chmod +x python_files/asm.py
 chmod +x python_files/iss.py
 
-mv python_files/asm.py ./asm
-mv python_files/iss.py ./iss
+mv python_files/asm.py python_files/asm
+mv python_files/iss.py python_files/iss
 
-echo "Installation terminée, essayez ./asm ou ./iss !"
+echo "Installation terminée, rendez-vous dans le dossier python_files et essayez ./asm ou ./iss !"
